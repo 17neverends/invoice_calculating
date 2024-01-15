23d2ba743d0c752e07721ec68941162206291e3e
17neverends <118381764+17neverends@users.noreply.github.com>
2024-01-15 16:30:48 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,11 +1,7 @@
 let tg = window.Telegram.WebApp;
 
-tg.expand(); 
-
-tg.MainButton.text = "Changed Text";
-tg.MainButton.setText("Changed Text1");
-tg.MainButton.textColor = "#F55353";
-tg.MainButton.color = "#143F6B";
-tg.MainButton.setParams({"color": "#143F6B"});
-tg.MainButton.enable();
-tg.MainButton.show();
+Telegram.WebApp.MainButton.setParams({
+  text_color: '#fff'
+}).onClick(Cafe.mainBtnClicked);
+Telegram.WebApp.BackButton.onClick(Cafe.backBtnClicked);
+Telegram.WebApp.setHeaderColor('bg_color');
