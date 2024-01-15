f9ce3d2d070408ef08bb00f5cb9cb9545754afea
17neverends <118381764+17neverends@users.noreply.github.com>
2024-01-15 14:31:10 +0300

    Update main.js

--- main.js
+++ main.js
@@ -1,3 +1,16 @@
+let tg = window.Telegram.WebApp;
+
+tg.expand(); 
+
+tg.MainButton.text = "Changed Text";
+tg.MainButton.setText("Changed Text1");
+tg.MainButton.textColor = "#F55353";
+tg.MainButton.color = "#143F6B";
+tg.MainButton.setParams({"color": "#143F6B"});
+tg.MainButton.enable()
+
+
+
 let input_contract_number = document.getElementById("contract_number");
 
 function validate_contract_number() {
